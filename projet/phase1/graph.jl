d3d36cadc8065cad2ed052cae3433a00b21acafe
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-09-15 22:52:27 -0400

    ajouter les fonctions pour les arêtes

--- projet/phase1/graph.jl
+++ projet/phase1/graph.jl
@@ -15,8 +15,9 @@ Exemple :
 Attention, tous les noeuds doivent avoir des données de même type.
 """
 mutable struct Graph{T} <: AbstractGraph{T}
-	name::String
-	nodes::Vector{Node{T}}
+	name :: String
+	nodes :: Vector{Node{T}}
+	edges :: Vector{Edge{T}}
 end
 
 """Ajoute un noeud au graphe."""
@@ -24,6 +25,13 @@ function add_node!(graph::Graph{T}, node::Node{T}) where T
 	push!(graph.nodes, node)
 	graph
 end
+
+"""Ajoute une arête au graphe."""
+function add_edge!(graph::Graph{T}, edge::Edge{T}) where T
+	push!(graph.edges, edge)
+	graph
+end
+
 
 # on présume que tous les graphes dérivant d'AbstractGraph
 # posséderont des champs `name` et `nodes`.
@@ -34,16 +42,27 @@ name(graph::AbstractGraph) = graph.name
 """Renvoie la liste des noeuds du graphe."""
 nodes(graph::AbstractGraph) = graph.nodes
 
+"""Renvoie la liste des arêtes du graphe."""
+edges(graph :: AbstractGraph) = graph.edges
+
 """Renvoie le nombre de noeuds du graphe."""
 nb_nodes(graph::AbstractGraph) = length(graph.nodes)
 
+"""Renvoie le nombre d'arêtes du graphe."""
+nb_edges(graph::AbstractGraph) = length(graph.edges)
+
 """Affiche un graphe"""
 function show(graph::Graph)
-	name = name(graph)
-	nb_nodes = nb_nodes(graph)
-	s = string("Graph ", name, " has ", nb_nodes, " nodes")
+	graph_name = name(graph)
+	graph_nb_nodes = nb_nodes(graph)
+	graph_nb_edges = nb_edges(graph)
+	s = string("Graph ", graph_name, " has ", graph_nb_nodes, " nodes and ",
+	graph_nb_edges, " edges")
 	for node in nodes(graph)
-		s = string(s, "\n", show(node))
+		show(node)
 	end
-	println(s)
+	for edge in edges(graph)
+		show(edge)
+	end
+	show(s)
 end

95a6efe7cc64ba8ddb0f2066523fab17c4ffec13
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-09-19 10:04:26 -0400

    Show function : change the printing (doesn't work with the given implementation because of the apparition of an array of type Nothing.
    Also changed a bit the name of the variables to make it more consistant instead of the functions :)

--- projet/phase1/graph.jl
+++ projet/phase1/graph.jl
@@ -44,23 +44,22 @@ nodes(graph::AbstractGraph) = graph.nodes
 nb_nodes(graph::AbstractGraph) = length(graph.nodes)
 
 """Renvoie la liste des arrêtes du graphe."""
-graph_edges(graph::AbstractGraph) = graph.edges
+edges(graph::AbstractGraph) = graph.edges
 
 """Renvoie le nombre de arrêtes du graphe."""
 nb_edges(graph::AbstractGraph) = length(graph.edges)
 
 """Affiche un graphe"""
 function show(graph::Graph)
-	name = name(graph)
-	nb_nodes = nb_nodes(graph)
-	nb_edges = nd_edges()
-	s = string("Graph ", name, " has ", nb_nodes, " nodes")
+	graph_name = name(graph)
+	graph_nb_nodes = nb_nodes(graph)
+	graph_nb_edges = nb_edges(graph)
+	println("Graph ", graph_name, " has ", graph_nb_nodes, " nodes")
 	for node in nodes(graph)
-		s = string(s, "\n", show(node))
+		show(node)
 	end
-	s = string(s, "\n", "and ", nb_edges, " edges")
+	println("and ", graph_nb_edges, " edges")
 	for edge in graph_edges(graph)
-		s = string(s, "\n", show(edge))
+		show(edge)
 	end
-	println(s)
 end

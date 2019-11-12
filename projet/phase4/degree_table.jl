b981fc146bd392ffbe65aabfba197da1ce4ff7a0
Arthur MARIE <arthur.marie@polymtl.ca>
2019-11-12 12:31:26 -0500

    Held&Karp - début

---
+++ projet/phase4/degree_table.jl
@@ -0,0 +1,33 @@
+import Base.show
+import Base.copy
+
+include(joinpath(@__DIR__, "..", "phase1", "graph.jl"))
+
+abstract type AbstractDegreeTable{T} end
+
+"""Structure de données associant à chaque noeud d'un graphe son noeud parent."""
+mutable struct DegreeTable{T} <: AbstractDegreeTable{T}
+    nodes::Vector{Node{T}}
+    degrees::Vector{Int64}
+end
+
+"""Renvoie la liste des noeuds."""
+nodes(degree_table::AbstractDegreeTable) = degree_table.nodes
+
+"""Renvoie la liste des degrés des noeuds."""
+degrees(degree_table::AbstractDegreeTable) = degree_table.degrees
+
+"""Initialise la table des degrés pour un graphe donné."""
+function init_degree_table(graph::AbstractGraph{T}) where T
+    return Vector{T}(nodes(graph), zeros(length(nodes(graph))))
+end
+
+"""Attribue à chaque noeud son degré dans le graphe en entrée."""
+function degree_table(graph::AbstractGraph)
+    degree_table = init_degree_table(graph)
+    for edge in edges(graph)
+        degrees(degree_table)[findfirst(name(s_node(edge)), name.(nodes(degree_table)))] += 1
+        degrees(degree_table)[findfirst(name(d_node(edge)), name.(nodes(degree_table)))] += 1
+    end
+    degree_table
+end
--- projet/phase4/main4.jl
+++ projet/phase4/main4.jl
@@ -1,10 +1,11 @@
 include(joinpath(@__DIR__, "..", "phase1", "graph.jl"))
 include(joinpath(@__DIR__, "..", "phase2", "parent_table.jl"))
 include(joinpath(@__DIR__, "..", "phase3", "priority_queue.jl"))
+include(joinpath(@__DIR__, "..", "phase4", "degree_table.jl"))
 
 """Renvoie une tournée optimisée passant par tous les points du graphe symétrique
 en entrée en utilisant l'algorithme de Rosenkrantz, Stearns et Lewis. Par défaut,
-le premier noeud du graphe est choisi comme point de départ.La méthode renvoie un 
+le premier noeud du graphe est choisi comme point de départ. La méthode renvoie un
 objet de type Graph contenant cette tournée.
 ATTENTION : dans le graphe en entrée, les noeuds doivent tous avoir un attribut "name" différent.
 """
@@ -41,4 +42,48 @@ function RSL_prim(graph::AbstractGraph{T}, starting_node::AbstractNode = nodes(g
     end
     # On renvoie la tournée optimisée :
     RSL_tree
+end
+
+"""À partir du graphe en entrée, construit et renvoie un 1-arbre de coût modifié minimum
+selon l'algorithme de Prim. Le coût modifié de chaque arête est le poids de cette arête
+plus le poids dans la liste "n_pi" de chacune de ses extrémités.
+"""
+function build_one_tree(graph::AbstractGraph, n_pi::Vector{Float64}, starting_node::AbstractNode = nodes(graph)[1])
+    edges_copy = copy(edges(graph))
+    for e in edges(graph)
+        weight(e) = weight(e) + n_pi[s_node(e)] + n_pi[dnode(e)]
+    end
+    one_tree = prim(graph, starting_node)
+    addedge!(one_tree, sort(filter(e -> !(e in edges(one_tree)), edges(graph)), by=weight)[1])
+    edges(graph) = edges_copy
+    one_tree
+end
+
+"""Calcule et renvoie la borne inférieure "w" que l'algorithme de Held et Karp cherche à maximiser."""
+function w(one_tree::AbstractGraph{T}, degree_table::AbstractDegreeTable{T}, n_pi::Vector{Float64}) where T
+    return graphweight(one_tree) + sum(degrees(degree_table)[i] * n_pi[i] for i = 1 : length(n_pi))
+end
+
+"""Renvoie une tournée optimisée passant par tous les points du graphe symétrique
+en entrée en utilisant l'algorithme de Held et Karp. La méthode renvoie un objet
+de type Graph contenant la meilleure tournée obtenue après "max_iter" itérations.
+ATTENTION : dans le graphe en entrée, les noeuds doivent tous avoir un attribut "name" différent.
+"""
+function HK(graph::AbstractGraph{T}, step::Float64 = , starting_node::AbstractNode = nodes(graph)[1], max_iter::Int64 = 100) where T
+    n_pi = Vector{Float64}(ones(length(nodes(graph))))
+    max_w = -Inf
+    max_n_pi = n_pi
+    iter = 0
+    while  && iter < max_iter
+        one_tree = build_one_tree(graph, n_pi, starting_node)
+        degree_table = degree_table(one_tree)
+        w = w(one_tree, degree_table, n_pi)
+        if w > max_w
+            max_w = w
+            max_n_pi = n_pi
+        end
+        n_pi += step * (degree_table - 2)
+
+
+    end
 end

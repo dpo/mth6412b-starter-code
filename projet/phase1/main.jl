3508642839cb0201835ce0762ae292ed64236669
Matteo Cacciola <matteo.cacciola@polymtl.ca>
2020-10-01 10:33:29 -0400

    adding right coordinates

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -11,7 +11,7 @@ include(joinpath(@__DIR__, "connected_component.jl"))
 # filename = joinpath(@__DIR__, "..\\..\\instances\\stsp\\gr17.tsp")
 
 # graph = build_graph(filename)
-lab_nodes = [Node("a",1),Node("b",2),Node("c",3),Node("d",4),Node("e",5),Node("f",6),Node("g",7),Node("h",8),Node("i",9)]
+lab_nodes = [Node("a",[-0.5,0.5]),Node("b",[0.0,1.0]),Node("c",[1.0,1.0]),Node("d",[2.0,1.0]),Node("e",[2.5,0.5]),Node("f",[2.0,0.0]),Node("g",[1.0,0.0]),Node("h",[0.0,0.0]),Node("i",[0.5,0.5])]
 lab_edges = [Edge(("a","b"),4),Edge(("a","h"),8),Edge(("b","c"),8),Edge(("b","h"),11),Edge(("c","d"),7),Edge(("c","f"),4),Edge(("c","i"),2),Edge(("d","e"),9),Edge(("d","f"),14),Edge(("e","f"),10),Edge(("f","g"),2),Edge(("g","h"),1),Edge(("g","i"),6),Edge(("h","i"),7)]
 graph = Graph("laboratory graph", lab_nodes, lab_edges)
 show(graph)

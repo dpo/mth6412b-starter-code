3e2e04dc11c0d60d65e4a1d818a1f206fe5d3d97
Xavier Lebeuf <reivaxo@hotmail.ca>
2021-09-20 11:44:38 -0400

    add runtest.jl

---
+++ runtests.jl
@@ -0,0 +1,8 @@
+using Test
+
+include("projet/phase1/edge.jl")
+include("projet/phase1/node.jl")
+include("projet/phase1/graph.jl")
+include("projet/phase1/read_stsp.jl")
+
+read_header(raw"instances/stsp/bays29.tsp")

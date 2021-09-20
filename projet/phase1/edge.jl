13a41b8d9ff0c1b5af83e267b80839e66109756d
Xavier Lebeuf <reivaxo@hotmail.ca>
2021-09-20 11:07:21 -0400

    Question 2
    ajout du type edge

---
+++ projet/phase1/edge.jl
@@ -0,0 +1,34 @@
+import Base.show
+
+"""Type abstrait dont d'autres types d'arètes dériveront."""
+abstract type AbstractEdge{T} end
+
+"""Type représentant les arètes d'un graphe.
+
+Exemple:
+
+        edge = Edge("Mtl_Qc", (1, 2), 20)
+
+"""
+mutable struct Edge{T} <: AbstractEdge{T}
+  name::String
+  data::Tuple{T, T}
+  weight::T
+end
+
+# on présume que tous les arètes dérivant d'AbstractEdge
+# posséderont des champs `name`, `data` et `weight`.
+
+"""Renvoie le nom de l'arète."""
+name(edge::AbstractEdge) = edge.name
+
+"""Renvoie les données contenues dans l'arète."""
+data(edge::AbstractEdge) = edge.data
+
+"""Renvoie les poids de l'arète."""
+weight(edge::AbstractEdge) = edge.weight
+
+"""Affiche une arète."""
+function show(edge::AbstractEdge)
+  println("Edge ", name(edge), ", data: ", data(edge), ", weight: ", weight(edge))
+end

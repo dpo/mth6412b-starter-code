29a21f387523002687d7dfb93c4d096809f59449
PhDGenieIndustriel12 <mahamadou.sarite@polymtl.ca>
2021-10-06 13:30:51 -0400

    composantes connexes

--- projet/phase2/main2.jl
+++ projet/phase2/main2.jl
@@ -12,16 +12,18 @@ function create_graph(filename::String)
         push!(lis_nodes,Node{Vector{Float64}}(string(k) , graph_nodes[k]))
     end
 
-    lis_edges = Edge[]
+    lis_edges = []
     for k =1 : length(graph_edges)
-        for l =1: length(graph_edges[k])
-            if graph_edges[k][l][2] != 0                                                   # On évite les répétitions des noeuds dans le graphe non orienté.
-            push!(lis_edges, Edge(string(k), string(graph_edges[k][l][1]) , graph_edges[k][l][2] ))
-            #sort![lis_edges(graph_edges[k][l][2])]
+        for l = 1: length(graph_edges[k])
+            if graph_edges[k][l][2] != 0                                                               # Les arêtes de poids nuls sont exclues.
+             push!(lis_edges, (string(k), string(graph_edges[k][l][1]) , graph_edges[k][l][2] ))  
             end 
         end
     end
-    Connex(filename, lis_nodes, lis_edges)
+    lis_edges
+    sort!(lis_edges, by = x -> x[3]); lis_edges                 # On tris les arêtes par ordre croissant de poids. 
+     
+    #Connex(filename, lis_nodes, lis_edges)
 end
 
 

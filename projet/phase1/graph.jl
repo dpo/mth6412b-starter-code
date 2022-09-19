d6f07544c7068484dd0515a10f374d480415e8fd
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-19 11:36:33 -0400

    Ajout edge dans graph

--- projet/phase1/graph.jl
+++ projet/phase1/graph.jl
@@ -19,8 +19,14 @@ function add_node!(graph::Graph{T}, node::Node{T}) where T
   graph
 end
 
+"""Ajoute une arête au graphe."""
+function add_edge!(graph::Graph{T}, edge::Edge{T}) where T
+  push!(graph.edges, edge)
+  graph
+end
+
 # on présume que tous les graphes dérivant d'AbstractGraph
-# posséderont des champs `name` et `nodes`.
+# posséderont des champs `name` et `nodes` et `edges`.
 
 """Renvoie le nom du graphe."""
 name(graph::AbstractGraph) = graph.name
@@ -31,10 +37,19 @@ nodes(graph::AbstractGraph) = graph.nodes
 """Renvoie le nombre de noeuds du graphe."""
 nb_nodes(graph::AbstractGraph) = length(graph.nodes)
 
+"""Renvoie la liste des arêtes du graphe."""
+nodes(graph::AbstractGraph) = graph.edges
+
+"""Renvoie le nombre d'arêtes du graphe."""
+nb_edges(graph::AbstractGraph) = length(graph.edges)
+
 """Affiche un graphe"""
 function show(graph::Graph)
-  println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes.")
+  println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes", " and ", nb_edges(graph), " edges.",)
   for node in nodes(graph)
     show(node)
+  end
+  for edge in edges(graph)
+    show(edge)
   end
 end

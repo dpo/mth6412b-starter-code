9a038fac30756dc1fc6b08e32ae148f99e955e1b
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-24 15:33:01 -0400

    Changements graph

--- projet/phase1/edge.jl
+++ projet/phase1/edge.jl
@@ -5,7 +5,7 @@ abstract type AbstractEdge{T} end
 
 mutable struct Edge{T} <: AbstractEdge{T}
   nodes::Tuple{Node{T}, Node{T}}
-  weight::Float64
+  weight::T
 end
 
 """Renvoie les extrémités d'une arête."""

81da516b5b3f1e14e15189f623fe2ef8b0e83df1
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-26 09:06:55 -0400

    changes

--- projet/phase1/make_graph.jl
+++ projet/phase1/make_graph.jl
@@ -3,17 +3,19 @@ function make_graph(filename::String)
     nodes_brut = read_nodes(header, filename)
     edges_brut = read_edges(header, filename)[1]
     weights = read_edges(header, filename)[2]
-    nodes=[]
-    edges=[]
-    for i in nodes_brut
-        node = Node(String(i), nodes_brut[i])
-        push!(nodes, node)
+    nodes=Array{Node, 1}(undef, length(nodes_brut))
+    edges=Array{Edge, 1}(undef, length(edges_brut))
+    j = 1
+    for k in keys(nodes_brut)
+        node = Node(string(k), nodes_brut[k])
+        nodes[j] = node
+        j = j + 1
     end
     for i = 1 : length(edges_brut)
-        n1 = Node(String(edges_brut[i][1]), edges_brut[i][1])
-        n2 = Node(String(edges_brut[i][2]), edges_brut[i][2])
+        n1 = Node(string(edges_brut[i][1]), edges_brut[i][1])
+        n2 = Node(string(edges_brut[i][2]), edges_brut[i][2])
         edge = Edge((n1, n2), weights[i])
-        push!(edges, edge)
+        edges[i] = edge
     end
     graph = Graph(filename, nodes, edges)
     return graph

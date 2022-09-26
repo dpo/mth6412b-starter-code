ab31b647fdc19163f81b08ce051a4280c3674ef8
Clélia Merel <cleliamerel@gmail.com>
2022-09-26 13:05:18 -0400

    Création d'un fichier main

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -4,4 +4,7 @@ include("read_stsp.jl")
 include("make_graph.jl")
 include("graph.jl")
 
-make_graph("instances/stsp/bayg29.tsp")
+
+function main(filename::String)
+    graph = make_graph("instances/stsp/bayg29.tsp")
+end

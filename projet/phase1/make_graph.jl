8d5405226f4dfc8efe5a1b72ee808f257245f1a1
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-26 08:33:37 -0400

    changes

--- projet/phase1/make_graph.jl
+++ projet/phase1/make_graph.jl
@@ -1,1 +1,20 @@
-function make_graph()
+function make_graph(filename::String)
+    header = read_header(filename)
+    nodes_brut = read_nodes(header, filename)
+    edges_brut = read_edges(header, filename)[1]
+    weights = read_edges(header, filename)[2]
+    nodes=[]
+    edges=[]
+    for i in nodes_brut
+        node = Node(String(i), nodes_brut[i])
+        push!(nodes, node)
+    end
+    for i = 1 : length(edges_brut)
+        n1 = Node(String(edges_brut[i][1]), edges_brut[i][1])
+        n2 = Node(String(edges_brut[i][2]), edges_brut[i][2])
+        edge = Edge((n1, n2), weights[i])
+        push!(edges, edge)
+    end
+    graph = Graph(filename, nodes, edges)
+    return graph
+end

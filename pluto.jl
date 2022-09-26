69d4c8bec0f6ec77fe0e7fee9e316e8baaa7600f
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-26 18:48:13 -0400

    changes pluto

--- pluto.jl
+++ pluto.jl
@@ -123,8 +123,30 @@ end
 # ╔═╡ 690bc9ef-640c-4f96-944f-7684b8311c6f
 md"""### Extension de la fonction *read_edges()*"""
 
+# ╔═╡ 094538a6-c9c9-4932-bc31-32a3b4697f2f
+md""" Au debut de la fonction (ligne 99), on crée un vecteur vide qui contiendra les poids des arêtes :
+```julia
+edges_weight = []
+```
+Juste avant de récupérer une arête (ligne 136), on récupère son poids en le convertissant en entier : 
+```julia
+weight = parse(Int,data[j+1])
+```
+Enfin, on ajoute ce poids à la liste des poids (ligne 151) :
+```julia
+push!(edges_weight, weight)
+```
+La fonction renvoie la liste des arêtes et la liste de leurs poids respectifs :
+```julia
+return edges, edges_weight
+```
+"""
+
 # ╔═╡ 6229c1f9-99bd-4723-a338-8b6e46eb2ef8
 md"""### Création de la fonction *make_graph()*"""
+
+# ╔═╡ bc2380ce-df43-46b1-84c7-f636f117d9e9
+
 
 # ╔═╡ 6898e60f-87ff-43cf-bc75-f6b7e2a26141
 md"""### Programme principal"""
@@ -1081,10 +1103,10 @@ version = "1.4.1+0"
 # ╟─775f23b8-3da3-11ed-1dab-2b8e6238fd87
 # ╟─042d77b3-a2cd-4c34-9386-c3bf50c01314
 # ╠═1bf9094d-82f5-40ae-ac55-a16e6bc5754f
-# ╟─bf88932f-a201-4e03-b6db-95d7be790ef4
-# ╟─a583132b-2ec1-4cb9-9c7a-fe5e2788a333
-# ╟─00407590-f3e0-4078-b229-b2203ed61583
-# ╟─76e663f2-9b13-4ee8-92ec-15a9ac430a59
+# ╠═bf88932f-a201-4e03-b6db-95d7be790ef4
+# ╠═a583132b-2ec1-4cb9-9c7a-fe5e2788a333
+# ╠═00407590-f3e0-4078-b229-b2203ed61583
+# ╠═76e663f2-9b13-4ee8-92ec-15a9ac430a59
 # ╟─5cb5ba5e-c685-43f6-9cf5-fafcbaa501c2
 # ╟─599b356c-bbf6-429e-ac27-0518a86c36a9
 # ╟─23ea2ccb-059b-4d33-b2bc-cc25827a5141
@@ -1092,7 +1114,9 @@ version = "1.4.1+0"
 # ╟─de1a416c-38dd-4008-abb6-5fcc2318a703
 # ╟─053ff6bd-47ce-42d3-86df-ba6822a33aaa
 # ╟─690bc9ef-640c-4f96-944f-7684b8311c6f
+# ╟─094538a6-c9c9-4932-bc31-32a3b4697f2f
 # ╟─6229c1f9-99bd-4723-a338-8b6e46eb2ef8
+# ╠═bc2380ce-df43-46b1-84c7-f636f117d9e9
 # ╟─6898e60f-87ff-43cf-bc75-f6b7e2a26141
 # ╠═8a7da04e-273f-41c7-af9b-8e61eef931a7
 # ╟─6d55b966-5f10-4c02-b52d-225376a1aaa3

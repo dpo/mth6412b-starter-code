89bc8b8340bc124d321dc7e9b2d5ca3cc8c97ef5
Vincent Vega <vincent.vega@polymtl.ca>
2022-09-26 11:13:10 -0400

    mise en page

--- pluto.jl
+++ pluto.jl
@@ -5,7 +5,22 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 775f23b8-3da3-11ed-1dab-2b8e6238fd87
+md"""## Projet du voyageur de commerce : Phase 1"""
 
+# ╔═╡ 5cb5ba5e-c685-43f6-9cf5-fafcbaa501c2
+md"""### Création du type *Edge*"""
+
+# ╔═╡ 23ea2ccb-059b-4d33-b2bc-cc25827a5141
+md"""### Extension du type *Graph*"""
+
+# ╔═╡ de1a416c-38dd-4008-abb6-5fcc2318a703
+md"""### Extension de la méthode *show*"""
+
+# ╔═╡ 690bc9ef-640c-4f96-944f-7684b8311c6f
+md"""### Extension de la fonction *read_edges()*"""
+
+# ╔═╡ 6229c1f9-99bd-4723-a338-8b6e46eb2ef8
+md"""### Création de la fonction *make_graph()*"""
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -25,5 +40,10 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 
 # ╔═╡ Cell order:
 # ╠═775f23b8-3da3-11ed-1dab-2b8e6238fd87
+# ╠═5cb5ba5e-c685-43f6-9cf5-fafcbaa501c2
+# ╠═23ea2ccb-059b-4d33-b2bc-cc25827a5141
+# ╠═de1a416c-38dd-4008-abb6-5fcc2318a703
+# ╠═690bc9ef-640c-4f96-944f-7684b8311c6f
+# ╠═6229c1f9-99bd-4723-a338-8b6e46eb2ef8
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002

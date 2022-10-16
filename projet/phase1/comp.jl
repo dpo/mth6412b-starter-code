21fb86978c52dbf322e465dd76065af180c48174
Vincent Vega <vincent.vega@polymtl.ca>
2022-10-15 20:12:24 -0400

    commentaires

--- projet/phase1/comp.jl
+++ projet/phase1/comp.jl
@@ -1,25 +1,36 @@
 using Test
 
-
+"""Type abstrait dont d'autres types de composantes connexes dériveront."""
 abstract type AbstractComp{T} end
 
+"""Type représentant les composantes connexes d'un graphe.
+
+root est le noeud-racine de la composante et les autres noeuds de la composante, 
+ainsi que le noeud-racine, sont dans children. 
+Chaque noeud est couplé à son parent. root est couplé à lui-même."""
 mutable struct Comp{T} <: AbstractComp{T}
     root::Node{T}
     children::Vector{Tuple{Node{T}, Node{T}}} #chaque enfant est un tuple de la forme (enfant, parent)
 end
 
+"""Renvoie la racine de la composante connexe"""
 root(comp::AbstractComp) = comp.root
 
+"""Renvoie les noeuds de la composante connexe"""
 children(comp::AbstractComp) = comp.children
 
+"""Fonction permettant de fusionner deux composantes connexes comp1 et comp2.
+
+Cette fonction modifie comp1 en lui ajoutant les noeuds de comp2.
+comp2 se place "sous" comp1 et la racine de comp1 devient la racine de la composante connexe finale."""
 function merge!(comp1::Comp{T}, comp2::Comp{T}) where T
     r1 = root(comp1)
     r2 = root(comp2)
     l1 = length(comp1.children)
     l2 = length(comp2.children)
-    for i = 1 : length(comp2.children)
+    for i = 1 : length(comp2.children) 
         r = comp2.children[i]
-        if r[1] != r2 
+        if r[1] != r2 #on ajoute tous les noeuds de comp2 à comp1 à l'exception de (r2, r2) car r1 sera la racine
             push!(comp1.children, r)
     
         end
@@ -29,6 +40,9 @@ function merge!(comp1::Comp{T}, comp2::Comp{T}) where T
     return comp1
 end
 
+"""Fonction permettant de vérifier si le noeud node est dans la composante connexe comp.
+
+La fonction parcourt la liste des enfants de comp."""
 function in_comp(comp::Comp{T}, node::Node{T}) where T
     for i = 1 : length(children(comp))
         if children(comp)[i][1] == node
--- projet/phase1/kruskal.jl
+++ projet/phase1/kruskal.jl
@@ -1,17 +1,20 @@
 include("comp.jl")
 
+"""Fonction renvoyant un arbre de recouvrement minimal pour le graphe graph à laide de
+l'algortihme de Kruskal."""
 function kruskal(graph::Graph{T}) where T
 
     number_of_edges = length(edges(graph))
     number_of_nodes = length(nodes(graph))
     @test number_of_nodes>0 #Vérifie que le graphe n'est pas vide
 
-    A = sort(edges(graph), by = x -> weight(x))
+    A = sort(edges(graph), by = x -> weight(x)) #liste des arêtes triées par poids
     @test length(A) == number_of_edges #Vérifie que A contient le bon nombre d'arrêtes
 
-    tree = []
-    liste_comp = []
+    tree = [] #contiendra la liste des arêtes de l'arbre de recouvrement minimal
+    liste_comp = [] #contiendra les composantes connexes du graphe
 
+    #on initie liste_comp avec chaque noeud qui est une composante connexe
     for i = 1 : number_of_nodes
         n = nodes(graph)[i]
         push!(liste_comp, Comp(n, [(n, n)]))
@@ -23,9 +26,9 @@ function kruskal(graph::Graph{T}) where T
 
         @test i == length(A) || weight(A[i]) <= weight(A[i+1]) #Vérifie que le poids de l'élément i de A est bien inférieur à celui du suivant si ce n'est pas le dernier
         n1, n2 = nodes(A[i])
-        k1 = findfirst(x -> in_comp(x, n1), liste_comp)
-        k2 = findfirst(x -> in_comp(x, n2), liste_comp)
-        if k1 != k2
+        k1 = findfirst(x -> in_comp(x, n1), liste_comp) #indice de la composante connexe contenant n1
+        k2 = findfirst(x -> in_comp(x, n2), liste_comp) #indice de la composante connexe contenant n2
+        if k1 != k2 #si n1 et n2 ne sont pas dans la même composante connexe
             push!(tree, A[i])
             c1 = liste_comp[k1]
             c2 = liste_comp[k2]

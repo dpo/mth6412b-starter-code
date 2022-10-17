3744a6f94a7c17e9b463c3742be8936e4f7825fb
Vincent Vega <vincent.vega@polymtl.ca>
2022-10-17 08:57:39 -0400

    bla

--- pluto.jl
+++ pluto.jl
@@ -208,6 +208,22 @@ main("instances/stsp/dantzig42.tsp")
 # ╔═╡ 093c6595-10ce-4619-9baa-d99657d017db
 main("instances/stsp/gr120.tsp")
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
 # ╟─775f23b8-3da3-11ed-1dab-2b8e6238fd87
 # ╟─042d77b3-a2cd-4c34-9386-c3bf50c01314
@@ -229,3 +245,5 @@ main("instances/stsp/gr120.tsp")
 # ╠═376c3bd3-1fbc-4f64-bb45-b02ef233dc35
 # ╠═f237e5f8-70bb-4f6d-8661-07295b7dfa20
 # ╠═093c6595-10ce-4619-9baa-d99657d017db
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
